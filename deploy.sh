546db7c29574bb60653e79c49401b7026db6c7a6
lance10030 <lance10030@163.com>
2021-01-25 15:44:31 +0800

    change deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 ###
  # @Author: lance
  # @Date: 2021-01-25 15:27:03
- # @LastEditTime: 2021-01-25 15:37:47
+ # @LastEditTime: 2021-01-25 15:44:21
  # @LastEditors: Please set LastEditors
  # @Description: In User Settings Edit
  # @FilePath: /system_dev/deploy.sh
@@ -10,7 +10,8 @@
 git pull 
 npm run build
 echo 'build finish'
-docker build -t razzil-front .
+docker rmi system_dev
+docker build -t system_dev .
 echo 'build images finish'
 docker-compose up -d
 echo 'all finished'
