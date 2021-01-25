a1c0a58c07edad78fdaf6958a9c27e00035466d6
lance10030 <lance10030@163.com>
2021-01-25 15:53:30 +0800

    change deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 ###
  # @Author: lance
  # @Date: 2021-01-25 15:27:03
- # @LastEditTime: 2021-01-25 15:44:21
+ # @LastEditTime: 2021-01-25 15:53:11
  # @LastEditors: Please set LastEditors
  # @Description: In User Settings Edit
  # @FilePath: /system_dev/deploy.sh
@@ -10,6 +10,8 @@
 git pull 
 npm run build
 echo 'build finish'
+docker stop system_dev
+docker rm system_dev
 docker rmi system_dev
 docker build -t system_dev .
 echo 'build images finish'
